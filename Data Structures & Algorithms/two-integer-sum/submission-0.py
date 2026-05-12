1e94311de7d1315c4391e0172460230f608eee3b
PaulS97 <71664329+PaulS97@users.noreply.github.com>
2026-05-12 14:44:33 -0400

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        storage = {}
+        final = []
+
+        for index, val in enumerate(nums):
+            compliment = target - val
+            if compliment in storage.keys():
+                return [storage[compliment], index]
+            else:
+                storage[val] = index
+        
+        return final
